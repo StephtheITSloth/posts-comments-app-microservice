c301931f8859c4bbca60fb55063497f54d35dfa5
stephtheitsloth <stephan.karim.sk@gmail.com>
2023-02-13 23:05:09 -0500

    implement event bus logic then wire post and comment servers

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 
@@ -16,13 +17,22 @@ app.get('/posts', (req,res)=>{
     res.send(posts)
 })
 
-app.post('/posts', (req,res) => {
+app.post('/posts', async (req,res) => {
     const id = randomBytes(4).toString('hex')
     const {title} = req.body
 
     posts[id] = {
         id,title
     }
+
+await axios('http://localhost:4005/events', {
+    type: "PostCreated",
+    data: {
+        id, title
+    }
+
+})
+
     res.send(posts[id])
 })
 
